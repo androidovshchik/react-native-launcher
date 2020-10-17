cedff4d8d50202f54847b21daac46c06e7e4365c
vlad <vladkalyuzhnyu@gmail.com>
2020-10-17 11:25:16 +0300

    changes example

--- android/src/main/java/defpackage/launcher/LauncherModule.kt
+++ android/src/main/java/defpackage/launcher/LauncherModule.kt
@@ -1,18 +1,55 @@
 package defpackage.launcher
 
+import android.annotation.TargetApi
+import android.app.Activity
 import android.app.AlarmManager
+import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.SystemClock
+import android.provider.Settings
 import androidx.core.app.AlarmManagerCompat
 import com.facebook.react.bridge.*
 import org.jetbrains.anko.alarmManager
 import org.jetbrains.anko.intentFor
 
-@Suppress("unused")
+@Suppress("unused", "DEPRECATION")
 class LauncherModule(reactContext: ReactApplicationContext) :
-    ReactContextBaseJavaModule(reactContext) {
+    ReactContextBaseJavaModule(reactContext), ActivityEventListener {
+
+    private var mPromise: Promise? = null
 
     override fun getName() = "LauncherPlugin"
+
+    @ReactMethod
+    fun canDrawOverlays(callback: Callback) {
+        with(reactApplicationContext) {
+            // Notice that it may be not working on Android O
+            // See https://stackoverflow.com/questions/46173460/why-in-android-8-method-settings-candrawoverlays-returns-false-when-user-has
+            callback.invoke(!isMarshmallowPlus() || Settings.canDrawOverlays(applicationContext))
+        }
+    }
+
+    @ReactMethod
+    @TargetApi(Build.VERSION_CODES.M)
+    fun requestDrawOverlays(promise: Promise?) {
+        with(reactApplicationContext) {
+            if (isMarshmallowPlus()) {
+                val activity = currentActivity
+                if (activity != null) {
+                    mPromise = promise
+                    addActivityEventListener(this@LauncherModule)
+                    activity.startActivityForResult(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION).apply {
+                        data = Uri.parse("package:$packageName")
+                    }, REQUEST_OVERLAY)
+                } else {
+                    promise?.reject("Activity is null")
+                }
+            } else {
+                promise?.resolve(Activity.RESULT_OK)
+            }
+        }
+    }
 
     @ReactMethod
     fun openExact(delay: Int, map: ReadableMap?) {
@@ -89,5 +126,33 @@ class LauncherModule(reactContext: ReactApplicationContext) :
                 )
             )
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+    }
+
+    override fun onActivityResult(
+        activity: Activity?,
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        if (requestCode == REQUEST_OVERLAY) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    mPromise?.resolve(resultCode)
+                }
+                Activity.RESULT_CANCELED -> {
+                    mPromise?.reject("Permission request was cancelled")
+                }
+            }
+            reactApplicationContext.removeActivityEventListener(this)
+            mPromise = null
+        }
+    }
+
+    companion object {
+
+        private const val REQUEST_OVERLAY = 100
     }
 }
